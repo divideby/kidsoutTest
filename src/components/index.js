001f609e5794da34273ac70031549537c85618f3
Dmitriy <divideby@yandex.ru>
2018-07-11 14:23:31 +0800

    add and move missing assets

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,10 @@
 import KidsoutText from "./KidsoutText";
+import KidsoutButton from "./KidsoutButton";
+import IconButton from "./IconButton";
+
 
 export {
     KidsoutText,
+    KidsoutButton,
+    IconButton
 }
