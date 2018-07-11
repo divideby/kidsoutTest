c8dc81d71de698d5517016d9fa7cf3c04840844b
Dmitriy <divideby@yandex.ru>
2018-07-11 14:24:28 +0800

    navigation header markup

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,13 +1,53 @@
-import { createStackNavigator } from 'react-navigation';
+import React from "react";
+import { View } from "react-native";
+import { createStackNavigator } from "react-navigation";
 
 import HomeScreen from "./screens/HomeScreen";
 import OrderScreen from "./screens/OrderScreen";
+import { IconButton } from "./components";
+import arrowBackIcon from "./components/assets/arrow-back/arrow-back.png";
+import circleAddIcon from "./components/assets/circle-add/circle-add.png";
+import favouriteIcon from "./components/assets/favourite/favourite.png";
 
-export default createStackNavigator({
-  Home: {
-    screen: HomeScreen
+export default createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen
+    },
+    Order: {
+      screen: OrderScreen,
+      navigationOptions: ({ navigation }) => ({
+        headerLeft: (
+          <IconButton
+            icon={arrowBackIcon}
+            onPress={() => navigation.goBack()}
+          />
+        ),
+        headerRight: (
+          <View style={{ display: "flex", flexDirection: "row" }}>
+            <IconButton icon={circleAddIcon} />
+            <IconButton icon={favouriteIcon} />
+          </View>
+        ),
+        title: "Подтверждение заказа"
+      })
+    }
   },
-  Order: {
-    screen: OrderScreen
-  },
-});
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "white",
+        shadowColor: "transparent",
+        borderBottomWidth: 1,
+        borderBottomColor: "#D9D9D9",
+        elevation: 0
+      },
+      headerTitleStyle: {
+        color: "black",
+        fontSize: 15,
+        fontFamily: "Lato-Regular",
+        fontWeight: "normal"
+      }
+    }
+  }
+);
